605ae34b9b91d55bb6c01d256657061c213ae786
adenikinju daniel <adenikinjudaniel@gmail.com>
2024-09-14 18:18:23 -0400

    initil commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors:{
+        LabelColor: "#777777",
+        BtnColor: "#3C41FF",
+        grey_500: "#201F24",
+        beige_500: "#98908B",
+        beige_100: "#F8F4F0"
+      }
+    },
+  },
+  plugins: [],
+};
